1c8f378c6bdce7e531201d495b387183f9c5295f
TuanBui <tuanbh92@gmail.com>
2018-03-21 11:14:14 +0700

    update Homepage

--- QLDVCD/src/homepage/Homepage.java
+++ QLDVCD/src/homepage/Homepage.java
@@ -5,6 +5,7 @@ public class Homepage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Homepage_access");
+		System.out.println("Login");
 	}
 
 }
